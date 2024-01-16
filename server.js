ca562355de1591cf7b6e1c197b1e339220ff9871
Nick Coats <coats.18@gmail.com>
2024-01-15 20:10:33 -0500

    moved port to env

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 import express from 'express';
 
-const app = express()
-const port = 3000;
-
 dotenv.config();
+
+const port = process.env.PORT;
+const app = express()
 
 // hubitat config
 const hub = {
@@ -13,19 +13,82 @@ const hub = {
     accessToken: process.env.HUB_ACCESS_TOKEN
 }
 
-app.get('/', (req, res) => {
-    res.send('Hellooo World!')
+app.get('/', async (req, res) => {
+    const devices = await getDevices();
+    console.dir(devices, {depth: null});
+    res.setHeader('Content-Type', 'text/html');
+    res.end(summaryView(devices))
 })
 
 app.get('/devices', async (req, res) => {
-    const response = await fetch(`http://${hub.host}/apps/api/43/devices/33?access_token=${hub.accessToken}`);
+    const response = await fetch(`http://${hub.host}/apps/api/43/devices?access_token=${hub.accessToken}`);
     const data = await response.json();
-
-    console.log(data);
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(data, null, 3));
 })
 
+app.get('/devices/:deviceId', async (req, res) => {
+    const response = await fetch(`http://${hub.host}/apps/api/43/devices/${req.params.deviceId}?access_token=${hub.accessToken}`);
+    const data = await response.json();
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(data, null, 3));
+})
+
+async function getDevices(){
+    const devicesResponse = await fetch(`http://${hub.host}/apps/api/43/devices?access_token=${hub.accessToken}`);
+    let devices = await devicesResponse.json();
+
+    for (let i = 0; i < devices.length; i++) {
+        let deviceMetrics = await fetch(`http://${hub.host}/apps/api/43/devices/${devices[i].id}?access_token=${hub.accessToken}`);
+        let metrics = await deviceMetrics.json();
+        devices[i].metrics = metrics.attributes;
+    }
+
+    return devices;
+}
+
+function metricsView(metrics){
+    let view = '';
+    metrics.forEach(metric => {
+        view += metric.name
+        view += ': '
+        view += metric.currentValue
+        view += '&nbsp;&nbsp;'
+    });
+    return view;
+}
+
+function summaryView(devices){
+    let viewHeader = 
+    `<html>
+        <body>`;
+
+    let viewFooter =
+    `</body>
+    </html>`
+
+    let view = '';
+    devices.forEach(device => {
+        view += `
+        <h2>${device.name}</h1>
+        <ul>
+            <li>Room: ${device.room}</li>
+            <li>Type: ${device.type}</li>
+            <li>Metrics: ${metricsView(device.metrics)}</li>
+        </ul>
+        `;
+    });
+
+    return `${viewHeader}${view}${viewFooter}`
+}
+
+app.get('/summary', async (req, res) => {
+    const devices = await getDevices();
+    console.dir(devices, {depth: null});
+    res.setHeader('Content-Type', 'text/html');
+    res.end(summaryView(devices))
+})
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Server listening on port ${port}....`)
 })
