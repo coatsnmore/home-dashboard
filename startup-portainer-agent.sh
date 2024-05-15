fc01bcbae9c2e97c9d19a3760d2c23df4a1ff677
coatsnmore <coats.18@gmail.com>
2024-05-15 16:05:26 -0400

    added portainer

---
+++ startup-portainer-agent.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# run on wsl in Windows
+docker run -d \
+  -p 9001:9001 \
+  --name portainer_agent \
+  --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /var/lib/docker/volumes:/var/lib/docker/volumes \
+  portainer/agent:2.19.5
